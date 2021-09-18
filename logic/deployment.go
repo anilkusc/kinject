906cce12c835e5ea5070ac735455a7a167770f02
anilkusc <anilkuscu95@gmail.com>
2021-09-18 13:55:17 +0300

    added some loggings

--- logic/deployment.go
+++ logic/deployment.go
@@ -1,6 +1,8 @@
 package logic
 
 import (
+	"fmt"
+
 	"github.com/anilkusc/kinject/kapi"
 	"k8s.io/client-go/kubernetes"
 )
@@ -12,12 +14,14 @@ func DeploymentEnvironmentSetter(client *kubernetes.Clientset, namespace string,
 			deployments := kapi.ListDeployments(client, namespace.Name)
 			for _, deployment := range deployments {
 				kapi.PatchDeploymentEnv(client, namespace.Name, deployment.Name, env)
+				fmt.Println("Environment Added: " + "Namespace: " + namespace.Name + "Name: " + deployment.Name + "Env: " + env)
 			}
 		}
 	} else {
 		deployments := kapi.ListDeployments(client, namespace)
 		for _, deployment := range deployments {
 			kapi.PatchDeploymentEnv(client, deployment.Namespace, deployment.Name, env)
+			fmt.Println("Environment Added: " + "Namespace: " + namespace + "Name: " + deployment.Name + "Env: " + env)
 		}
 	}
 }
@@ -28,12 +32,14 @@ func DeploymentEnvironmentRemover(client *kubernetes.Clientset, namespace string
 			deployments := kapi.ListDeployments(client, namespace.Name)
 			for _, deployment := range deployments {
 				kapi.DeleteDeploymentEnv(client, namespace.Name, deployment.Name, env)
+				fmt.Println("Environment Deleted: " + "Namespace: " + namespace.Name + "Name: " + deployment.Name + " Env: " + env)
 			}
 		}
 	} else {
 		deployments := kapi.ListDeployments(client, namespace)
 		for _, deployment := range deployments {
 			kapi.DeleteDeploymentEnv(client, deployment.Namespace, deployment.Name, env)
+			fmt.Println("Environment Deleted: " + "Namespace: " + namespace + "Name: " + deployment.Name + " Env: " + env)
 		}
 	}
 }
